f4035221bec34e55da2878c83842ef31b7e37a0f
makrenka <smokrenko79@gmail.com>
2023-03-22 20:41:53 +0300

    add try...catch to the getResource

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,13 +3,17 @@ export class MarvelService {
     _apiKey = 'apikey=053a5108c61bc76fdb70a11474fa008d';
 
     getResource = async (url) => {
-        let res = await fetch(url);
+        try {
+            let res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+            }
 
-        return await res.json();
+            return await res.json();
+        } catch (err) {
+            console.log(err);
+        };
     };
 
     getAllCharacters = () => {
